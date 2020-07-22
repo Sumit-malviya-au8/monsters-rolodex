550f9e6e42fc1e8051426f71cec6cbda1026ec6e
Sumit Malviya <sumitindian11@gmail.com>
2020-07-22 21:52:55 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import { CardList } from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  //Executing part of code after mounting of the render method
+  //ComponentDidMount is method accessible bcoz of class.
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      //  .then(user => console.log(user))
+      .then(users => this.setState({ monsters: users }))
+  }
+
+  handleChange = e => {
+    this.setState({searchField:e.target.value})
+  }
+
+
+  //Render method comes with the Component menthod which is the part of React Lib.
+  //setState with one argument in async while setstate whnever take call back function it become syschronus
+  // () => console.log(this.state) second arg of setState
+  render() {
+    const { monsters, searchField } = this.state;
+    
+    //Dynamic Search:- Rerendring bcoz of setState
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+
+    return (
+      <div className="App">
+        <h1>MONSTERS ROLODEX</h1>
+        <SearchBox 
+        placeholder="Search Monsters"
+        handleChange={this.handleChange} />
+        <CardList monsters={filteredMonsters} />
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
+
